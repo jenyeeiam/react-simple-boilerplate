535f708e31827fd7f457b7993f5ffad0617e38a5
Jen Yee <jenyee1022@yahoo.com>
2016-10-23 00:06:45 +0000

    working todo app with redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,58 @@
 import React, {Component} from 'react';
+import GreenButton from './GreenButton.jsx'
+import List from './List.jsx'
 
-class App extends Component {
+const App = React.createClass ({
+
+  getInitialState() {
+    return {
+      value: "Green Button",
+      formText: ""
+    }
+  },
+
+  componentWillMount() {
+    console.log(store.getState());
+  },
+
+  componentDidMount() {
+
+    store.subscribe(this.stateChanged);
+  },
+
+
+  stateChanged() {
+    this.setState({value: store.getState()});
+  },
+
+  clickFun() {
+    // console.log(this.state);
+    store.dispatch({type: 'BANANAS'});
+    if (this.state.value === 'I love BANANAS') {
+      store.dispatch({type: 'ORANGES'});
+    }
+  },
+
   render() {
+    const { store } = this.context;
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <GreenButton
+        value={this.state.value}
+        clickFun={this.clickFun}
+        />
+        {/* <List
+        // this is my object error
+          // store={this.props.store}
+          formText={this.state.formText}
+          // stateChanged={this.stateChanged}
+        /> */}
+      </div>
     );
   }
-}
+});
+App.contextTypes = {
+  store: React.Proptypes.object
+};
+
 export default App;
--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,44 @@
 // Application entrypoint.
-
 // Load up the application styles
 require("../styles/application.scss");
 
 // Render the top-level React component
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App.jsx';
+import { createStore, combineReducers } from 'redux';
+import { Provider } from 'react-redux';
+import bananas from './reducers/bananas.js';
+import form from './reducers/form.jsx';
+//import App from './App.jsx';
 
-ReactDOM.render(<App />, document.getElementById('react-root'));
+//////////////
+import { addTodo, toggleTodo, setVisibilityFilter, VisibilityFilters } from './actions';
+import todoApp from './reducers.js';
+import App from './components/App.js';
+
+let store = createStore(todoApp);
+console.log(store.getState());
+let unsubscribe = store.subscribe(() =>
+  console.log(store.getState())
+);
+store.dispatch(addTodo('Learn some redux'));
+
+
+var reducers = combineReducers({
+  bananaState: bananas,
+  // formState: form
+});
+
+
+store.subscribe(() => {
+  console.log(store.getState());
+});
+
+
+//var store = createStore(reducers);
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('react-root')
+);
